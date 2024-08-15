7ae679b6b507545596939f982a4890729f194021
Marcos <marcoscardoso.programmer@gmail.com>
2024-08-15 17:30:34 -0300

    second commit

--- java/concurrent/FileSimilarity.java
+++ java/concurrent/FileSimilarity.java
@@ -1,7 +1,10 @@
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.Semaphore;
 
 public class FileSimilarity {
+
+    private static Map<String, List<Long>> fileFingerprints = new HashMap<>();;
 
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
@@ -9,14 +12,18 @@ public class FileSimilarity {
             System.exit(1);
         }
 
-        // Create a map to store the fingerprint for each file
-        Map<String, List<Long>> fileFingerprints = new HashMap<>();
+        for (int i = 0; i < 5; i++) {
+            
+        }
 
         // Calculate the fingerprint for each file
         for (String path : args) {
-            List<Long> fingerprint = fileSum(path);
-            fileFingerprints.put(path, fingerprint);
+            // List<Long> fingerprint = fileSum(path);
+            FileSumThread fileSumThread = new FileSumThread(path);
+            fileSumThread.start();
         }
+
+        
 
         // Compare each pair of files
         for (int i = 0; i < args.length; i++) {
@@ -31,27 +38,58 @@ public class FileSimilarity {
         }
     }
 
-    private static List<Long> fileSum(String filePath) throws IOException {
-        File file = new File(filePath);
-        List<Long> chunks = new ArrayList<>();
-        try (FileInputStream inputStream = new FileInputStream(file)) {
-            byte[] buffer = new byte[100];
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                long sum = sum(buffer, bytesRead);
-                chunks.add(sum);
+    static class FileSumThread extends Thread {
+
+        private String filePath;
+        Semaphore mutex = new Semaphore(1);
+        Map<String, List<Long>> fingerprints;
+
+        public FileSumThread(String filePath) {
+            this.filePath = filePath;
+        }
+
+        @Override
+        public void run() {
+            try {
+                this.fileSum();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
-        return chunks;
+
+        private void fileSum() throws IOException, InterruptedException {
+            mutex.acquire();
+            File file = new File(this.filePath);
+            List<Long> chunks = new ArrayList<>();
+            try (FileInputStream inputStream = new FileInputStream(file)) {
+                byte[] buffer = new byte[100];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    long sum = sum(buffer, bytesRead);
+                    chunks.add(sum);
+                }
+            } catch (Exception e) {            System.out.println(chunks);
+
+                System.out.println(e.getMessage());
+            }
+            System.out.println(this.filePath);
+            // System.out.println(chunks);
+            fileFingerprints.put(this.filePath, chunks);
+            System.out.println(fileFingerprints.get(this.filePath));
+            mutex.release();
+        }
+
+        private static long sum(byte[] buffer, int length) {
+            long sum = 0;
+            for (int i = 0; i < length; i++) {
+                sum += Byte.toUnsignedInt(buffer[i]);
+            }
+            return sum;
+        }
+
     }
 
-    private static long sum(byte[] buffer, int length) {
-        long sum = 0;
-        for (int i = 0; i < length; i++) {
-            sum += Byte.toUnsignedInt(buffer[i]);
-        }
-        return sum;
-    }
+
 
     private static float similarity(List<Long> base, List<Long> target) {
         int counter = 0;
